e8a6fff05daada27294a3911d5c736bc4bf78d97
Adarsh Kumar Sinha <aksinha.dhn@gmail.com>
2021-05-18 15:45:14 +0530

    added bullet added background

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
+import random
 
 # Initialize the pygame
 pygame.init()
 
 # Create the screen
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption("John Wick 4")
-
 
 # Player
 playerImg = pygame.image.load('player.png')
@@ -17,10 +17,38 @@ playerY = 480
 playerX_change = 0
 playerY_change = 0
 
+# Enemy
+gunImg = pygame.image.load('gun.png')
+gunX = random.randint(0, 736)
+gunY = 50
+gunX_change = 0.4
+gunY_change = 0.4
+
+# Bullet
+bulletImg = pygame.image.load('bullets.png')
+bulletX = 0
+bulletY = 0
+bulletX_change = 0
+bulletY_change = -0.4
+bullet_state = "ready"
+
+# backgrounf
+backgorund = pygame.image.load('background.png')
+
 
 def Player(x, y):
     # Draw
-    screen.blit(playerImg, (x,y))
+    screen.blit(playerImg, (x, y))
+
+
+def Gun(x, y):
+    screen.blit(gunImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x+16, y+10))
 
 
 # Game loop for the main window
@@ -28,13 +56,11 @@ running = True
 while running:
 
     screen.fill((90, 112, 22))
-
-
+    screen.blit(backgorund, (0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
 
         # If key keystroke is pressed check whether its right or left
 
@@ -47,6 +73,11 @@ while running:
                 playerY_change = -1
             if event.key == pygame.K_DOWN:
                 playerY_change = 1
+            if event.key == pygame.K_SPACE :
+                if bullet_state == "ready":
+                    bulletX = playerX
+                    bulletY = playerY
+                    fire_bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 playerX_change = 0
@@ -54,7 +85,29 @@ while running:
 
     playerX += playerX_change
     playerY += playerY_change
-    if playerY >= 536 or playerY <= 0: playerY_change = 0
+
+    gunY_change = 0
+
+    if gunX >= 736:
+        gunX_change = -0.4
+        gunY_change = 10
+    if gunX <= 0:
+        gunX_change = 0.4
+        gunY_change = 10
+    if playerY >= 536 or playerY <= 150: playerY_change = 0
     if playerX >= 736 or playerX <= 0: playerX_change = 0
+
+    gunX += gunX_change
+    gunY += gunY_change
+
+    bulletY += bulletY_change
+
+    # Bullet movement
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
+        if bulletY < -10: bullet_state = "ready"
+
     Player(playerX, playerY)
+    Gun(gunX, gunY)
+
     pygame.display.update()
